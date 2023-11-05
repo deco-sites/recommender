d09b37ab3dac78832ccfdddfb0405c0e9921f5f3
viniciusventura29 <viniciusventura29@gmail.com>
2023-11-05 14:58:52 -0300

    not showing trend topics if products length equal to 0

--- sections/Product/MostSeen.tsx
+++ sections/Product/MostSeen.tsx
@@ -76,34 +76,42 @@ export default function MostSeen({
   products,
 }: SectionProps<typeof loader>) {
   return (
-    <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-      <h2 class="text-2xl leading-8 lg:leading-10 text-base-content text-center lg:text-4xl">
-        {title}
-      </h2>
-      <h3 class="leading-6 lg:leading-8 text-neutral text-center lg:text-2xl">
-        {subTitle}
-      </h3>
+    <>
+      {products.length <= 0 ? (
+        <div></div>
+      ) : (
+        <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+          <h2 class="text-2xl leading-8 lg:leading-10 text-base-content text-center lg:text-4xl">
+            {title}
+          </h2>
+          <h3 class="leading-6 lg:leading-8 text-neutral text-center lg:text-2xl">
+            {subTitle}
+          </h3>
 
-      <div class="grid grid-cols-1 mt-8 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-        {products.map((p) => (
-          <a href={p?.url} class="group border rounded-md relative">
-            <div class='p-4'>
-            <div class='text-white bg-red-500 px-2 py-1 text-xs rounded absolute top-3 left-2'>{p?.off}% Off</div>
-            <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-              <img
-                src={p?.image}
-                alt="Tall slender porcelain bottle with natural clay textured body and cork stopper."
-                class="h-full w-full object-cover object-center group-hover:opacity-75"
-              />
-            </div>
-            <h3 class="mt-4 text-sm text-gray-700">{p?.title}</h3>
-            <p class="mt-1 text-lg font-medium text-gray-900">
-            R$&nbsp;{p?.salesPrice}
-            </p>
-            </div>
-          </a>
-        ))}
-      </div>
-    </div>
+          <div class="grid grid-cols-1 mt-8 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+            {products.map((p) => (
+              <a href={p?.url} class="group border rounded-md relative">
+                <div class="p-4">
+                  <div class="text-white bg-red-500 px-2 py-1 text-xs rounded absolute top-3 left-2">
+                    {p?.off}% Off
+                  </div>
+                  <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+                    <img
+                      src={p?.image}
+                      alt="Tall slender porcelain bottle with natural clay textured body and cork stopper."
+                      class="h-full w-full object-cover object-center group-hover:opacity-75"
+                    />
+                  </div>
+                  <h3 class="mt-4 text-sm text-gray-700">{p?.title}</h3>
+                  <p class="mt-1 text-lg font-medium text-gray-900">
+                    R$&nbsp;{p?.salesPrice}
+                  </p>
+                </div>
+              </a>
+            ))}
+          </div>
+        </div>
+      )}
+    </>
   );
 }
